a5152ca5d5cc600709b3463ca68bae08634d74cc
ldjking <ldjbox@126.com>
2015-04-14 09:11:19 +0800

    格式化代码

--- gruntfile.js
+++ gruntfile.js
@@ -18,27 +18,27 @@ module.exports = function(grunt) {
 
 
   grunt.registerTask('cssbuild', 'build css files', function(arg1, arg2) {
-	//需要处理的文件 包括jsp和html
+    //需要处理的文件 包括jsp和html
     var files = grunt.file.expand(["src/**/*.jsp", "src/**/*.html"]);
     console.log("待处理文件", files);
     for (var i = 0; i < files.length; i++) {
-      parseJsp(files[i]);//逐项处理
+      parseJsp(files[i]); //逐项处理
     }
   });
 
-  function parseCssPath(file, cssFile) {//解析CSS路径
+  function parseCssPath(file, cssFile) { //解析CSS路径
     var path = require("path");
-	var cssPath;//待返回的cssPath
-	
-	cssFile=cssFile||"";
-	cssFile=cssFile.replace(/\?.*$/,"");//清除后面已有的query版本号
-	console.log("cssFile",cssFile);
-    if (/^\$/g.test(cssFile)) {//这里要判断路径类型，jsp的是绝对路径
-	  cssPath=cssFile.replace(/^\$.*?[\/|\\]/g,"src/");//替换规则
-	  //todo 这里写替换规则
+    var cssPath; //待返回的cssPath
+
+    cssFile = cssFile || "";
+    cssFile = cssFile.replace(/\?.*$/, ""); //清除后面已有的query版本号
+    console.log("cssFile", cssFile);
+    if (/^\$/g.test(cssFile)) { //这里要判断路径类型，jsp的是绝对路径
+      cssPath = cssFile.replace(/^\$.*?[\/|\\]/g, "src/"); //替换规则
+      //todo 这里写替换规则
       return cssPath;
-    }else{
-	  cssPath = path.join(file, "../",cssFile);
+    } else {
+      cssPath = path.join(file, "../", cssFile);
       return cssPath;
     }
   }
@@ -48,26 +48,26 @@ module.exports = function(grunt) {
 
     var cssFiles = fileContent.match(/<link.*?rel="stylesheet".*?>/ig);
     //console.log("cssFiles", cssFiles);
-    var mapings=[];
+    var mapings = [];
     for (var i = 0; i < cssFiles.length; i++) {
       var cssHref = (/href=['"](.*)?['"]/ig).exec(cssFiles[i]);
-      var maping=buildCss(file, cssHref);
+      var maping = buildCss(file, cssHref);
       mapings.push(maping);
     }
 
 
-    var afterRevision=addJSPRevision(file,fileContent,mapings);//修改版本号的jsp内容
-    var destFile=file.replace("src","dist");//修改版本号后的文件存放位置  todo  要改成真实的规则
-	
-    grunt.file.write(destFile,afterRevision);//保存构建后的文件
-    return mapings;//返回资源文件MD5映射
+    var afterRevision = addJSPRevision(file, fileContent, mapings); //修改版本号的jsp内容
+    var destFile = file.replace("src", "dist"); //修改版本号后的文件存放位置  todo  要改成真实的规则
+
+    grunt.file.write(destFile, afterRevision); //保存构建后的文件
+    return mapings; //返回资源文件MD5映射
   }
 
   function buildCss(file, cssHref) {
-    var CleanCSS=require("clean-css");
-    var MD5=require("MD5");
-    var PATH=require("path");
-    cssHref=(cssHref&&cssHref[1])||"";
+    var CleanCSS = require("clean-css");
+    var MD5 = require("MD5");
+    var PATH = require("path");
+    cssHref = (cssHref && cssHref[1]) || "";
     var cssPath = parseCssPath(file, cssHref);
     var cssSrc = cssPath.replace(/\\/g, "/");
 
@@ -78,7 +78,7 @@ module.exports = function(grunt) {
       sourceMap: false,
       shorthandCompacting: false,
       sourceMap: false,
-      keepBreaks:true,
+      keepBreaks: true,
       advanced: false,
       aggressiveMerging: false,
       debug: false,
@@ -87,12 +87,12 @@ module.exports = function(grunt) {
       rebase: true
     };
 
-    var cssDir=PATH.dirname(cssSrc);//css文件所在目录
-    options.relativeTo=cssDir;//css合并时的图片相对路径重新构建
+    var cssDir = PATH.dirname(cssSrc); //css文件所在目录
+    options.relativeTo = cssDir; //css合并时的图片相对路径重新构建
 
 
     try {
-      var availableFiles=[cssSrc];
+      var availableFiles = [cssSrc];
       compiled = new CleanCSS(options).minify(availableFiles);
     } catch (err) {
       grunt.log.error(err);
@@ -100,28 +100,28 @@ module.exports = function(grunt) {
     }
 
     var compiledCssString = compiled.styles;
-    var md5Str=MD5(compiledCssString);
+    var md5Str = MD5(compiledCssString);
 
 
-    var shortMd5=md5Str.substr(0,6);
-    var cssDest = cssSrc.replace("src", "dist").replace(".css","."+shortMd5+".css");
+    var shortMd5 = md5Str.substr(0, 6);
+    var cssDest = cssSrc.replace("src", "dist").replace(".css", "." + shortMd5 + ".css");
 
     //console.log("shortMd5",shortMd5);
     //console.log("cssDest",cssDest);
 
-    grunt.file.write(cssDest, compiledCssString);//产生了一个新的css文件
+    grunt.file.write(cssDest, compiledCssString); //产生了一个新的css文件
     //修改jsp的css文件版本号  当有多个css文件时  要增量替换
     return {
-      key:cssHref,
-      value:cssHref.replace(".css","."+shortMd5+".css").replace(/\?.*?$/g,"")
+      key: cssHref,
+      value: cssHref.replace(".css", "." + shortMd5 + ".css").replace(/\?.*?$/g, "")
     };
   }
 
-  function addJSPRevision(file,fileContent,mapings){//给jsp文件css引用加上版本号
+  function addJSPRevision(file, fileContent, mapings) { //给jsp文件css引用加上版本号
     //console.log("maping",mapings);
-    for(var i=0;i<mapings.length;i++){
-      var map=mapings[i];
-      fileContent=fileContent.replace(map.key,map.value);
+    for (var i = 0; i < mapings.length; i++) {
+      var map = mapings[i];
+      fileContent = fileContent.replace(map.key, map.value);
     }
     return fileContent;
   }
